fb15bd7d82f1aa44ac3c7209a03a265e385a724a
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2024-02-05 14:07:59 +0530

    fixed production failure

--- src/routes/eventsData.js
+++ src/routes/eventsData.js
@@ -302,7 +302,6 @@ export const eventsData = [
         description:
           "There would be tournaments in popular games like Valorant PC, BGMI etc. Also there would be a wide range of tournaments and leagues for all skill levels. Whether you’re a beginner or a pro, you can find the perfect competition to challenge yourself and show off your skills. Not to mention there would be a variety of rewards and prizes for the top players.",
         image: "/event-images/e-sports.png",
-          "There would be tournaments in popular games like Valorant PC, COD mobile etc. Also there would be a wide range of tournaments and leagues for all skill levels. Whether you’re a beginner or a pro, you can find the perfect competition to challenge yourself and show off your skills. Not to mention there would be a variety of rewards and prizes for the top players.",
         image: "/event-images/e_sports.png",
         link: "https://unstop.com/p/e-sports-international-institute-of-information-technology-iiit-bhubaneswar-858272",
         linkText: "Register",
@@ -317,6 +316,7 @@ export const eventsData = [
         link: "https://unstop.com/events/e-sports-valorant-advaita-techno-cultural-fest-international-institute-of-information-technology-iiit-bhubaneswar-887248",
         linkText: "Register",
         prize: 10000
+      }
     ],
   },
   {
