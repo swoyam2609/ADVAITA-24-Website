8ece416604cb791dd8737cdeffb617f3f0226aec
raj-alpha-swain <rajalphaswain786@gmail.com>
2024-02-27 02:05:00 +0530

    change in pop up

--- src/components/merchPopup/index.js
+++ src/components/merchPopup/index.js
@@ -7,16 +7,16 @@ import { useState } from "react";
 
 export default function MerchPopup(props) {
     const [menuOpen, setMenuOpen] = useState(true);
-    return (
-        <>
-        {menuOpen?
-        <div className={classes.popup}>
-            <div className={classes.popupText}>Wrap yourself in the symphony of stardust and wear the magic of stellar night with our Stellar Reverie Collection!</div>
-            <HashLink to="/merch#" onClick={()=>setMenuOpen(false)}>
-                <AngleButton text="MERCHANDISE" />
-            </HashLink>
-            <img onClick={()=>setMenuOpen(false)} src={iconClose} alt="close" />
-        </div>:null}
-        </>
-  );
+//     return (
+//         <>
+//         {menuOpen?
+//         <div className={classes.popup}>
+//             <div className={classes.popupText}>Wrap yourself in the symphony of stardust and wear the magic of stellar night with our Stellar Reverie Collection!</div>
+//             <HashLink to="/merch#" onClick={()=>setMenuOpen(false)}>
+//                 <AngleButton text="MERCHANDISE" />
+//             </HashLink>
+//             <img onClick={()=>setMenuOpen(false)} src={iconClose} alt="close" />
+//         </div>:null}
+//         </>
+//   );
 }
